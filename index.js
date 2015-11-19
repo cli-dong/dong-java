293b6cdc3b5d48e47fa2283420a68a5b221886c4
crossjs <liwenfu@crossjs.com>
2015-11-19 20:36:36 +0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 /*
  * dong-java
- * https://github.com/aoiuym/dong-java
+ * https://github.com/crossjs/dong-java
  *
- * Copyright (c) 2015 aoiuym
- * Licensed under the ISC license.
+ * Copyright (c) 2015 crossjs
+ * Licensed under the MIT license.
  */
 
 'use strict';
 
 module.exports = {
   command: 'java',
-  description: '更新java部分',
-  // options: [],
-  bootstrap: require('./lib/latest-java')
+  description: '生成 API 文档',
+  options: [{
+    name: 'debug',
+    alias: 'd',
+    description: '显示调试信息',
+    defaults: false
+  }],
+  bootstrap: require('./lib/java'),
+  strict: true
 }
